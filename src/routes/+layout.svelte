ff3a6f165126e3f98f4cb469d4a6e1a5812275a5
ananastya1 <nastya10_tv@mail.ru>
2023-07-12 14:16:55 +0300

    Fix

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,20 +1,20 @@
 <script lang="ts">
-  import { base } from '$app/paths';
+	import { base } from '$app/paths';
 </script>
 
 <header>
-  <nav>
-    <a href="{base}/">Home</a>
-    <!-- <a href="{base}#skills">Skills</a>
+	<nav>
+		<a href="{base}/">Home</a>
+		<!-- <a href="{base}#skills">Skills</a>
     <a href="{base}#projects">Projects</a> -->
-    <a href="{base}/comic">Comic</a>
-  </nav>
+		<a href="{base}/comic">Comic</a>
+	</nav>
 </header>
 
 <main>
-  <slot />
+	<slot />
 </main>
 
 <footer>
-  <p>&copy; 2023 Anastasia Barabanova. All rights reserved.</p>
+	<p>&copy; 2023 Anastasia Barabanova. All rights reserved.</p>
 </footer>
