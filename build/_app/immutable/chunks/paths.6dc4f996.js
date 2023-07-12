adcef658605d9506f1f78a98649595f20c2c63ac
ananastya1 <nastya10_tv@mail.ru>
2023-07-12 15:42:20 +0300

    Fix

---
+++ build/_app/immutable/chunks/paths.6dc4f996.js
@@ -0,0 +1,1 @@
+var s;const t=((s=globalThis.__sveltekit_1wqcedv)==null?void 0:s.base)??"/Portfolio_SvelteKit";var e;const a=((e=globalThis.__sveltekit_1wqcedv)==null?void 0:e.assets)??t;export{a,t as b};
